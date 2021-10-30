fb5e7d717eb527033a50e8d745d4353924731ee0
Eric-unix <84213432+Eric-unix@users.noreply.github.com>
2021-10-30 14:05:29 -0500

    Cambio de idioma

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() , TextToSpeech.OnInitListener {
     }
 
     private fun speak(){
-        var message: String = findViewById<TextView>(R.id.tvStatus).text.toString()
+        var message: String = findViewById<TextView>(R.id.etMessage).text.toString()
 
         tts!!.speak(message, TextToSpeech.QUEUE_FLUSH,null , "")
     }
@@ -31,7 +31,7 @@ class MainActivity : AppCompatActivity() , TextToSpeech.OnInitListener {
     override fun onInit(status: Int) {
         if (status== TextToSpeech.SUCCESS){
             findViewById<TextView>(R.id.tvStatus).text = "Hello Kotlin!"
-            tts!!.setLanguage(Locale.US)
+            tts!!.setLanguage(Locale("ES"))
         }else{
             findViewById<TextView>(R.id.tvStatus).text = "No disponible :("
         }
